1f9f4511ae93914d2517087e4cf4c04b28d93b6e
iverson-ykh <915131918@qq.com>
2022-08-10 12:52:36 +0800

    pvz-sun

---
+++ PVZ/Assets/Script/SkySunManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkySunManager : MonoBehaviour
+{
+    private GameObject Prefab_Sun;
+    private float createSunPosY=6;
+
+    private float sunTopMin=6;
+    private float sunBottomMax = -3.5f;
+    
+    private float sunLeftMin = -5f;
+    private float sunRightMax = 8f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Prefab_Sun = Resources.Load<GameObject>("Sun");
+        InvokeRepeating("CreateSun", 3,3);
+    }
+    public void CreateSun() {
+        float horizontalPos = Random.Range(sunBottomMax,sunTopMin);
+        float verticalPos = Random.Range(sunLeftMin,sunRightMax);
+        
+        Sun sun=GameObject.Instantiate<GameObject>(Prefab_Sun,Vector3.zero,Quaternion.identity,transform).GetComponent<Sun>();
+        sun.InitForSky(verticalPos,horizontalPos,createSunPosY);
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ PVZ/Assets/Script/Sun.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sun : MonoBehaviour
+{
+
+    private float horizontalTarget;
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y<=this.horizontalTarget) {
+            Invoke("DestroySun",10);
+            return;
+        }
+        transform.Translate(Vector3.down*Time.deltaTime);
+    }
+    public void InitForSky(float horizontal,float generatePosX,float generatePosY) {
+        this.horizontalTarget = horizontal;
+        transform.position = new Vector2(generatePosX, generatePosY);
+        
+    }
+    public void DestroySun() {
+        Destroy(gameObject);
+    }
+}
